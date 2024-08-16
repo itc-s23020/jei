ccaf10d59933a7acdbdae258d6aa39ac1c424347
itc-s23020 <s23020@std.it-college.ac.jp>
2024-08-16 13:50:07 +0000

    d51Q26kadai

---
+++ d51Q26kadai.java
@@ -0,0 +1,14 @@
+// s23020
+// 演算子を使うプログラム。
+
+public class d51Q26kadai {
+    public static void main(String[] args) {
+        int n = 1;
+        String s = switch (n) {
+            case 1 -> "one";
+            case 2 -> "two";
+            default -> "?";
+        };
+        System.out.println(s);
+    }
+}
