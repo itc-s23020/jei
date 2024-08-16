0542e954468a78a8c79593274fe04492fa5e5df9
itc-s23020 <s23020@std.it-college.ac.jp>
2024-08-16 13:45:13 +0000

    d50Q6kadai

--- d50Q6kadai.java
+++ d50Q6kadai.java
@@ -1,12 +1,10 @@
 // s23020
-// 指定された範囲内の素数を求め、その素数の数も出力するプログラム
+// 素数の数を出力するム
 
-public class d50Q6kadai {
+class d50Q6kadai {
     public static void main(String[] args) {
         int min = Integer.parseInt(args[0]);
         int max = Integer.parseInt(args[1]);
-
-        //入れ替える 
         if (max < min) {
             int tmp = max;
             max = min;
@@ -16,28 +14,25 @@ public class d50Q6kadai {
         PrimeNumbers primeNumbers = new PrimeNumbers();
         primeNumbers.initializePrimeNumbers(max);
         int[] part = primeNumbers.getPart(min, max);
-        // 素数
-        for (int prime : part) {
+
+        // 素数の数を出力
+        System.out.printf("素数の数: %d%n", part.length);
+
+        for (int prime : part){
             System.out.printf("%d, ", prime);
         }
-        System.out.println(); // 改行
-
-        // 素数を出力
-        System.out.printf("%dから%dまでの素数の数は%d個%n", min, max, part.length);
     }
 }
 
 class PrimeNumbers {
     boolean[] isPrimeNumbers;
 
-    // 素数を求める
     void initializePrimeNumbers(int max) {
         isPrimeNumbers = new boolean[max + 1];
         for (int i = 2; i <= max; i++) {
             isPrimeNumbers[i] = true;
         }
 
-        // 素数判定
         for (int p = 2; p <= Math.sqrt(max); p++) {
             if (isPrimeNumbers[p]) {
                 for (int m = p * 2; m <= max; m += p) {
@@ -47,7 +42,6 @@ class PrimeNumbers {
         }
     }
 
-    // 指定された範囲の素数を取得
     int[] getPart(int min, int max) {
         int count = 0;
         for (int n = min; n <= max; n++) {
